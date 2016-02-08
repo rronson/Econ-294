26a740415c190a7c2116be011356f2229ca7df7b
rronson <rronson@ucsc.edu>
2016-02-07 21:28:24 -0800

    test

---
+++ Assignments/RichardRonsonAssignment4.R
@@ -0,0 +1,9 @@
+#Question 0
+firstname<-"Richard"
+lastname<-"Ronson"
+studentID<-1505029
+assignment<-"Econ 294A Assignment 4"
+print(paste(firstname,lastname))
+print(studentID)
+print(assignment)
+#Question 1
